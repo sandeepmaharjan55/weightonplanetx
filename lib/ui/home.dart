41eb4a6e8fa55da6e4dfd88e9cccc2cc6e40fe2c
sandeepmaharjan55 <sandeepmaharjan55@gmail.com>
2020-07-12 09:38:16 +0545

    info added

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -6,11 +6,35 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final TextEditingController _weightController = new TextEditingController();
   int radioValue = 0;
+  double _finalResult = 0.0;
+  String _formattedText = "";
   void handleRadioValueChanged(int value) {
     setState(() {
       radioValue = value;
-      print(radioValue);
+      // print(radioValue);
+      switch (radioValue) {
+        case 0:
+          _finalResult = calculateWeight(_weightController.text, 1.12);
+          _formattedText =
+              "Your Weight on Neptune is ${_finalResult.toStringAsFixed(1)}";
+          // print(_finalResult);
+          break;
+        case 1:
+          _finalResult = calculateWeight(_weightController.text, 0.38);
+          // print(_finalResult);
+          _formattedText =
+              "Your Weight on Mars is ${_finalResult.toStringAsFixed(1)}";
+          break;
+        case 2:
+          _finalResult = calculateWeight(_weightController.text, 0.91);
+          // print(_finalResult);
+          _formattedText =
+              "Your Weight on Venus is ${_finalResult.toStringAsFixed(1)}";
+          break;
+        default:
+      }
     });
   }
 
@@ -39,11 +63,11 @@ class _HomeState extends State<Home> {
                 child: new Column(
                   children: [
                     new TextField(
-                      controller: null,
+                      controller: _weightController,
                       keyboardType: TextInputType.number,
                       decoration: new InputDecoration(
                           labelText: "Your Weight on Earth",
-                          hintText: "In Kg",
+                          hintText: "In Pound",
                           icon: new Icon(Icons.person_outline)),
                     ),
                     new Padding(
@@ -54,30 +78,44 @@ class _HomeState extends State<Home> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         new Radio<int>(
+                            activeColor: Colors.brown,
                             value: 0,
                             groupValue: radioValue,
                             onChanged: handleRadioValueChanged),
                         new Text(
-                          "Pluto",
-                          style: new TextStyle(color: Colors.white),
+                          "Neptune",
+                          style: new TextStyle(color: Colors.white30),
                         ),
                         new Radio<int>(
+                            activeColor: Colors.red,
                             value: 1,
                             groupValue: radioValue,
                             onChanged: handleRadioValueChanged),
                         new Text(
                           "Mars",
-                          style: new TextStyle(color: Colors.white),
+                          style: new TextStyle(color: Colors.white30),
                         ),
                         new Radio<int>(
+                            activeColor: Colors.orangeAccent,
                             value: 2,
                             groupValue: radioValue,
                             onChanged: handleRadioValueChanged),
                         new Text(
                           "Venus",
-                          style: new TextStyle(color: Colors.white),
+                          style: new TextStyle(color: Colors.white30),
                         ),
                       ],
+                    ),
+                    new Padding(padding: const EdgeInsets.all(15.6)),
+                    //result text area
+                    new Text(
+                      _weightController.text.isEmpty
+                          ? "Please enter weight"
+                          : "$_formattedText lbs",
+                      style: new TextStyle(
+                          color: Colors.white,
+                          fontSize: 19.0,
+                          fontWeight: FontWeight.w500),
                     )
                   ],
                 ),
@@ -91,5 +129,14 @@ class _HomeState extends State<Home> {
           // ),
           ),
     );
+  }
+
+  double calculateWeight(String weight, double multiplier) {
+    if (int.parse(weight).toString().isNotEmpty && int.parse(weight) > 0) {
+      return int.parse(weight) * multiplier;
+    } else {
+      print("wrong!!!");
+      return int.parse("180") * 0.38;
+    }
   }
 }
